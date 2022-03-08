7d6a5ad5bc726be358aa35c4e6c0c15dfec7ae34
Mostafa Karimi <mkarimi21@hotmail.com>
2022-03-08 13:24:56 +0330

    define car class

--- section03rev2/bin/car.dart
+++ section03rev2/bin/car.dart
@@ -4,5 +4,33 @@ class Car{
     String carColor = "White";
     String carModel = "IranKhodro";
     int carEngine = 12452;
-    
+
+}
+
+class Pride extends Car{
+    @override
+  // TODO: implement carColor
+  String get carColor => "Red";
+
+    @override
+  // TODO: implement carModel
+  String get carModel => "Saipa";
+
+    @override
+  // TODO: implement carEngine
+  int get carEngine => 22332;
+}
+
+class Pegue extends Car{
+    @override
+  // TODO: implement carColor
+  String get carColor => "Blue";
+
+    @override
+  // TODO: implement carModel
+  String get carModel => "IranKhodro";
+
+    @override
+  // TODO: implement carEngine
+  int get carEngine => 4471;
 }
