088d1109081f74a87cc838333ad5ed00fdde1121
Mostafa Karimi <mkarimi21@hotmail.com>
2022-03-12 01:09:28 +0330

    add new class for learning

---
+++ section05rev02/lib/secend_class.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class SecendClass extends StatefulWidget {
+  const SecendClass({Key? key}) : super(key: key);
+
+  @override
+  State<SecendClass> createState() => _SecendClassState();
+}
+
+class _SecendClassState extends State<SecendClass> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
