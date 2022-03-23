24fea39409b47362806d02a4afa363de664251f4
Mostafa Karimi <mkarimi21@hotmail.com>
2022-03-24 00:40:30 +0430

    add column rectangle for column testing shape

--- section05rev02/lib/widgets/row_widget.dart
+++ section05rev02/lib/widgets/row_widget.dart
@@ -12,20 +12,49 @@ class _RowWidgetState extends State<RowWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Container"),
+          title: Text("Row and Column"),
         ),
         body: Container(
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.5,
-            margin: EdgeInsets.all(15),
-            decoration: BoxDecoration(
-                color: Colors.orange,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10))),
-            child: Text("Hello Mostafa"),
-          ),
+          padding: EdgeInsets.all(9),
+          child: Column(children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 110,
+              color: Colors.purple,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: List.generate(
+                    3,
+                    (index) => Container(
+                          width: 40,
+                          height: 40,
+                          margin: EdgeInsets.only(right: 3),
+                          color: Colors.white,
+                          child: Center(child: Text("${index + 1}")),
+                        )).toList(),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 15),
+              width: 150,
+              height: 250,
+              color: Colors.black38,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: List.generate(
+                    3,
+                    (index) => Container(
+                          width: 40,
+                          height: 40,
+                          margin: EdgeInsets.only(right: 3),
+                          color: Colors.white,
+                          child: Center(child: Text("${index + 1}")),
+                        )).toList(),
+              ),
+            ),
+          ]),
         ));
   }
 }
