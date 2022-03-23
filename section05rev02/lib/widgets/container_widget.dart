1d1cca350f3cc8daed0d8b2a591d12fdf54c41d5
Mostafa Karimi <mkarimi21@hotmail.com>
2022-03-24 00:01:57 +0430

    add container pages

--- section05rev02/lib/widgets/container_widget.dart
+++ section05rev02/lib/widgets/container_widget.dart
@@ -11,12 +11,21 @@ class _ContainerWidgetState extends State<ContainerWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Container"),
-      ),
-      body: Center(
-        child: Container(),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Container"),
+        ),
+        body: Container(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.5,
+            margin: EdgeInsets.all(15),
+            decoration: BoxDecoration(
+                color: Colors.orange,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10))),
+            child: Text("Hello Mostafa"),
+          ),
+        ));
   }
 }
