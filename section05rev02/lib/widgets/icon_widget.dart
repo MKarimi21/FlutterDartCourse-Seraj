432aa020f32a4d88f20cb99b4274b88c376ead3c
Mostafa Karimi <mkarimi21@hotmail.com>
2022-03-24 18:03:58 +0430

    add show messenger  after click

--- section05rev02/lib/widgets/icon_widget.dart
+++ section05rev02/lib/widgets/icon_widget.dart
@@ -16,8 +16,37 @@ class _IconWidgetState extends State<IconWidget> {
       ),
       body: Container(
         padding: EdgeInsets.all(9),
-        child: Container(),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.alternate_email_outlined,
+                color: Colors.blueAccent,
+                size: 80,
+              ),
+              IconButton(
+                onPressed: _onClickedIcon,
+                icon: Icon(
+                  Icons.send_outlined,
+                  color: Colors.blue,
+                  size: 60,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  void _onClickedIcon() {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(
+      "ارسال انجام شد",
+      textDirection: TextDirection.rtl,
+    )));
   }
 }
