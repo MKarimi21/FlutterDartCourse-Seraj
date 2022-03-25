148e9f0caae819eb2db67d37a7d0b94933230b20
Mostafa Karimi <mkarimi21@hotmail.com>
2022-03-25 22:38:57 +0430

    change container to image

--- section05rev02/lib/widgets/animation_container_widget.dart
+++ section05rev02/lib/widgets/animation_container_widget.dart
@@ -24,68 +24,79 @@ class _AnimationContainerWidgetState extends State<AnimationContainerWidget> {
         padding: EdgeInsets.all(9),
         child: Container(
           width: MediaQuery.of(context).size.width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: _width,
-                height: _height,
-                color: _color,
-              ),
-              Container(
-                width: 221,
-                height: 42,
-                padding: EdgeInsets.symmetric(horizontal: 5),
-                margin: EdgeInsets.symmetric(vertical: 7),
-                decoration:
-                    BoxDecoration(border: Border.all(color: Colors.green)),
-                child: Center(
-                    child: TextField(
-                        keyboardType: TextInputType.number,
-                        controller: _widthController,
-                        decoration: InputDecoration(
-                            border: InputBorder.none, hintText: "Width Size"))),
-              ),
-              Container(
-                width: 221,
-                height: 42,
-                padding: EdgeInsets.symmetric(horizontal: 5),
-                margin: EdgeInsets.symmetric(vertical: 7),
-                decoration:
-                    BoxDecoration(border: Border.all(color: Colors.green)),
-                child: Center(
-                    child: TextField(
-                        keyboardType: TextInputType.number,
-                        controller: _heightController,
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Height Size"))),
-              ),
-              Container(
-                width: 151,
-                height: 42,
-                padding: EdgeInsets.symmetric(horizontal: 5),
-                margin: EdgeInsets.symmetric(vertical: 7),
-                decoration: BoxDecoration(color: Colors.red),
-                child: InkWell(
-                  onTap: () {
-                    String _widthString = _widthController.text;
-                    _width = double.parse(_widthString);
-
-                    String _heightString = _heightController.text;
-                    _width = double.parse(_heightString);
-
-                    setState(() {});
-                  },
-                  child: Center(
-                      child: Text(
-                    "Submit",
-                    style: TextStyle(color: Colors.white),
-                  )),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                AnimatedContainer(
+                  width: _width,
+                  height: _height,
+                  color: _color,
+                  duration: Duration(seconds: 1),
+                  child: Image.asset(
+                    "assets/images/avatar.png",
+                    width: _width,
+                    height: _height,
+                    fit: BoxFit.fill,
+                  ),
                 ),
-              ),
-            ],
+                Container(
+                  width: 221,
+                  height: 42,
+                  padding: EdgeInsets.symmetric(horizontal: 5),
+                  margin: EdgeInsets.symmetric(vertical: 7),
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.green)),
+                  child: Center(
+                      child: TextField(
+                          keyboardType: TextInputType.number,
+                          controller: _widthController,
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Width Size"))),
+                ),
+                Container(
+                  width: 221,
+                  height: 42,
+                  padding: EdgeInsets.symmetric(horizontal: 5),
+                  margin: EdgeInsets.symmetric(vertical: 7),
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.green)),
+                  child: Center(
+                      child: TextField(
+                          keyboardType: TextInputType.number,
+                          controller: _heightController,
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Height Size"))),
+                ),
+                Container(
+                  width: 151,
+                  height: 42,
+                  padding: EdgeInsets.symmetric(horizontal: 5),
+                  margin: EdgeInsets.symmetric(vertical: 7),
+                  decoration: BoxDecoration(color: Colors.red),
+                  child: InkWell(
+                    onTap: () {
+                      String _widthString = _widthController.text;
+                      _width = double.parse(_widthString);
+
+                      String _heightString = _heightController.text;
+                      _height = double.parse(_heightString);
+
+                      _color = Colors.yellow;
+                      setState(() {});
+                    },
+                    child: Center(
+                        child: Text(
+                      "Submit",
+                      style: TextStyle(color: Colors.white),
+                    )),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
