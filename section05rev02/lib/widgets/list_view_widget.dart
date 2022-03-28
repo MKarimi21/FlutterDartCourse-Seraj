934322df4acf2b1fd1da44fddafa5f1cb10a8d62
Mostafa Karimi <mkarimi21@hotmail.com>
2022-03-28 19:52:28 +0430

    update list view

--- section05rev02/lib/widgets/list_view_widget.dart
+++ section05rev02/lib/widgets/list_view_widget.dart
@@ -8,44 +8,50 @@ class ListViewWidget extends StatefulWidget {
 }
 
 class _ListViewWidgetState extends State<ListViewWidget> {
+  final List<String> _name = [
+    "Sajjad",
+    "Naser",
+    "Ghasem",
+    "Mahmood",
+    "Hamed",
+    "Ali",
+    "Abolfazl",
+    "Mahdi",
+    "Mostafa",
+    "Reza",
+    "Ahmad",
+  ];
   dynamic _selected;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("List View"),
-        ),
-        body: Container(
-            padding: EdgeInsets.all(10),
-            child: SingleChildScrollView(
-              child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: List.generate(12, (index) {
-                      bool _select = (_selected == index);
-                      return ListTile(
-                        title: Text("Person"),
-                        subtitle: Text("About Person"),
-                        leading: Icon(Icons.person),
-                        trailing: Wrap(
-                          spacing: 5,
-                          children: [
-                            Icon(Icons.favorite),
-                            Icon(Icons.call),
-                            Icon(Icons.message),
-                            Icon(Icons.delete),
-                          ],
-                        ),
-                        selected: _select,
-                        onTap: () {
-                          _selected = index;
-                          setState(() {});
-                        },
-                      );
-                    }).toList(),
-                  )),
-            )));
+      appBar: AppBar(
+        title: Text("List View"),
+      ),
+      body: Container(
+          padding: EdgeInsets.all(10),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            child: ListView.builder(
+                itemCount: _name.length, itemBuilder: _listViewItemBuilder),
+          )),
+    );
+  }
+
+  Widget _listViewItemBuilder(BuildContext context, int index) {
+    return ListTile(
+      title: Text(_name.elementAt(index)),
+      subtitle: Text("Number ${index + 1}"),
+      leading: Icon(Icons.person),
+      trailing: Wrap(
+        spacing: 5,
+        children: [
+          Icon(Icons.favorite),
+          Icon(Icons.call),
+          Icon(Icons.message),
+          Icon(Icons.delete),
+        ],
+      ),
+    );
   }
 }
