b726c1cdc899027f952479d760788fe45f67c3a7
Mostafa Karimi <mkarimi21@hotmail.com>
2022-03-31 21:05:18 +0430

    update dialog

--- section05rev02/lib/note/widgets/first_page_dialog.dart
+++ section05rev02/lib/note/widgets/first_page_dialog.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
 
 class FirstPageDialog extends StatelessWidget {
-  const FirstPageDialog({Key? key}) : super(key: key);
-
+  String _name = "";
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 350,
-      height: 150,
-      color: Colors.white,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text("لطفا اسم خود را وارد کنید:"),
-        ],
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        padding: EdgeInsets.all(5),
+        width: 350,
+        height: 150,
+        color: Colors.white,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("لطفا اسم خود را وارد کنید:"),
+            TextField(
+              onChanged: (changed) {
+                _name = changed;
+              },
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.3,
+              height: 35,
+              margin: EdgeInsets.only(top: 10),
+              child: MaterialButton(
+                onPressed: () {},
+                child: Text(
+                  "تایید",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              color: Colors.red,
+            ),
+          ],
+        ),
       ),
     );
   }
