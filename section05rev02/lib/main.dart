44102164b1b6b013b51959ae18a0e71121cca063
Mostafa Karimi <mkarimi21@hotmail.com>
2022-03-31 18:57:09 +0430

    add new page on main page

--- section05rev02/lib/main.dart
+++ section05rev02/lib/main.dart
@@ -300,3 +300,36 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+class MainPage extends StatefulWidget {
+  MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "لطفا مسیر خود را انتخاب کنید",
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.green,
+        centerTitle: true,
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
